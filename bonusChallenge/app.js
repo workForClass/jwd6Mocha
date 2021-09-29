6b549c7eea69a327b85ea8b4e500e0890de09b19
Lisa Ostman <peolina@live.com.au>
2021-09-29 16:32:46 +1000

    added more instructions to bonusChallenge

--- bonusChallenge/app.js
+++ bonusChallenge/app.js
@@ -1,22 +1,23 @@
 /*
  * You should write Mocha code that will test the functionalities of fizzBuzz() and gatherNamesOf() functions.
- ** Don't forget to initialize npm, install Mocha, and change the scripts in the package.json to run the test suite <command npm> test.
- *** Try to use TDD - test code, implement code, refactor the code.
+ ** Start with looking at the test/app-test.js!!!
  */
 
 /*
   * Write a program that prints the numbers. 
-      1 - numbers multiples of three prints “Fizz” instead of the number 
-      2 - numbers multiples of five prints “Buzz” instead of the number. 
-      3 - numbers which are multiples of three and five prints “FizzBuzz”
-      4 -  and numbers that are either multiple of three or five prints the number.
+      1 - numbers that are multiples of three returns “Fizz” instead of the number 
+      2 - numbers that are multiples of five returns “Buzz” instead of the number. 
+      3 - numbers which are multiples of three and five returns “FizzBuzz”
+      4 -  and numbers that are either multiple of three or five returns the number.
       Ex: 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, ...
+      Write the code as you complete each it test in the test/app-test.js. 
  */
 const fizzBuzz = () => {};
 
 /*Implement the gatherNamesOf function as needed to guarantee that:
   1- the function receives an array of objects with people names as a parameter and returns the property name value.
   Ex: The array of objects is: [{name: 'John'}, {name:'Patricia'}]. It should return ['John', 'Patricia'];
+  Write the code as you complete each it test in the test/app-test.js. 
 */
 const gatherNamesOf = () => {};
 
--- bonusChallenge/test/app-test.js
+++ bonusChallenge/test/app-test.js
@@ -1,15 +1,39 @@
+// Step 1. First and foremost, set up mocha in this folder!
+// How to: Remember to npm init, npm install mocha -D, add script test line to package.json
+// Follow the Google doc steps of how you did in the activity folder if you forget.
+
 //Import assert
+const assert = require("assert");
 
 //Import functions from app.js
+const { fizzBuzz, gatherNamesOf } = require("../app");
+
+describe("tests for the fizzBuzz function", () => {
+
+})
 
 /*
-  * Create unit tests for FizzBuzz
-  1 - You should create an unit test for each functionality of the FizzBuzz function
+  * Step 2. Create unit tests for FizzBuzz. 
+  You should create an unit test for each functionality of the FizzBuzz function. 
+  Unit tests to write: 
+    1. it should return Fizz when the value is multiple of 3 but not of 5
+    2. it should return Buzz when the value is multiple of 5 but not of 3
+    3. it should return FizzBuzz when the value is multiple of 3 and 5
+    4. should return the number given when the value is not multiple of 3 or 5
+  NOTE: After each unit test, use it to guide you on what code you need to write in app.js 
+  to pass the test and make sure it passes (run npm test) before writing the next it test.
+  Don't forget to use the steps of setup, exercise and verify to guide you in writing tests. 
 */
 
-/* Create a unit test for the gatherNamesOf function.
-  1- It should should return an array
-  2- It should give me the same output length as the input
-  3- It should give me the property name value of the object passed in
-  * CHALLENGE: use beforeEach method to write a more concise test code.
+describe("tests for the gatherNamesOf function", () => {
+
+})
+
+/* Step 3. Create unit tests for the gatherNamesOf function. Unit tests to write:
+    1. It should should return an array
+    2. It should give me the same output length as the input
+    3. It should give me the property name value of the object passed in
+  NOTE: After each unit test, use it to guide you on what code you need to write in app.js to 
+  pass the test and make sure it passes (run npm test) before writing the next it test.
+  * CHALLENGE: use beforeEach method to write a more concise test code and refactor code that gets repeated in each test.
  */
